343a0747ab2b52753e3a6b50eb40b6e41dc428c1
Marc-Olivier Derouin <marc-olivier.derouin@polymtl.ca>
2023-02-13 18:12:41 -0500

    Adding wait action

--- src/extension.ts
+++ src/extension.ts
@@ -45,68 +45,85 @@ export function activate(context: vscode.ExtensionContext) {
 
 		let scriptPath = editor.document.uri.fsPath;
 		let scriptCwd = dirname(scriptPath);
-		let process = spawn(pythonPath, [scriptPath, '--extension'], { cwd: scriptCwd });
 
-		const datas: string[] = [];
-		process.stdout.on('data', data => {
-			datas.push(`${data}`);
+		vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification
+		}, async (progress) => {
+
+			progress.report({ message: "Running tests..." });
+
+			await new Promise<void>((resolve, reject) => {
+				
+				let process = spawn(pythonPath, [scriptPath, '--extension'], { cwd: scriptCwd });
+
+				const datas: string[] = [];
+				process.stdout.on('data', data => {
+					datas.push(`${data}`);
+				});
+		
+				process.on('exit', () => {
+					let output = datas.pop();
+					if(!output) {
+						vscode.window.showErrorMessage('Error with response from test. Please check that you have school-grader-grader installed.');
+						return reject();
+					}
+					let jsonObject: any;
+					try{
+						jsonObject = JSON.parse(output);
+					}catch(e){
+						vscode.window.showErrorMessage('Error with response from test. Please check that you have school-grader-grader installed.');
+						return reject();
+					}
+		
+					let lineNumbers = Object.keys(jsonObject).map((key) => parseInt(key, 10));
+		
+					// Get the decorations for the success lines
+					let successDecorations = lineNumbers.filter((lineNumber) => {
+						return jsonObject[lineNumber].status === 'success';
+					}).map((lineNumber) => {
+						let line = editor.document.lineAt(lineNumber - 1);
+						return {
+							range: new vscode.Range(line.range.start, line.range.end),
+							hoverMessage: jsonObject[lineNumber].message
+						};
+					});
+					
+					// Get the decorations for the failure lines
+					let failureDecorations = lineNumbers.filter((lineNumber) => {
+						return jsonObject[lineNumber].status === 'failure';
+					}).map((lineNumber) => {
+						let line = editor.document.lineAt(lineNumber - 1);
+						return {
+							range: new vscode.Range(line.range.start, line.range.end),
+							hoverMessage: jsonObject[lineNumber].message
+						};
+					});
+					
+					// Get the decorations for the error lines
+					let errorDecorations = lineNumbers.filter((lineNumber) => {
+						return jsonObject[lineNumber].status === 'error';
+					}).map((lineNumber) => {
+						let line = editor.document.lineAt(lineNumber - 1);
+						return {
+							range: new vscode.Range(line.range.start, line.range.end),
+							hoverMessage: jsonObject[lineNumber].message
+						};
+					});
+					
+					// Apply the decorations to the active text editor
+					editor.setDecorations(successDecorationType, successDecorations);
+					editor.setDecorations(failureDecorationType, failureDecorations);
+					editor.setDecorations(errorDecorationType, errorDecorations);
+					return resolve();
+					
+				});
+	
+			});
+
+			vscode.window.showInformationMessage('Tests completed.');
+			
 		});
-
-		process.on('exit', () => {
-			let output = datas.pop();
-			if(!output) {
-				vscode.window.showErrorMessage('Error with response from test. Please check that you have school-grader-grader installed.');
-				return;
-			}
-			let jsonObject: any;
-			try{
-				jsonObject = JSON.parse(output);
-			}catch(e){
-				vscode.window.showErrorMessage('Error with response from test. Please check that you have school-grader-grader installed.');
-				return;
-			}
-
-			let lineNumbers = Object.keys(jsonObject).map((key) => parseInt(key, 10));
-
-			// Get the decorations for the success lines
-			let successDecorations = lineNumbers.filter((lineNumber) => {
-				return jsonObject[lineNumber].status === 'success';
-			}).map((lineNumber) => {
-				let line = editor.document.lineAt(lineNumber - 1);
-				return {
-					range: new vscode.Range(line.range.start, line.range.end),
-					hoverMessage: jsonObject[lineNumber].message
-				};
-			});
-			
-			// Get the decorations for the failure lines
-			let failureDecorations = lineNumbers.filter((lineNumber) => {
-				return jsonObject[lineNumber].status === 'failure';
-			}).map((lineNumber) => {
-				let line = editor.document.lineAt(lineNumber - 1);
-				return {
-					range: new vscode.Range(line.range.start, line.range.end),
-					hoverMessage: jsonObject[lineNumber].message
-				};
-			});
-			
-			// Get the decorations for the error lines
-			let errorDecorations = lineNumbers.filter((lineNumber) => {
-				return jsonObject[lineNumber].status === 'error';
-			}).map((lineNumber) => {
-				let line = editor.document.lineAt(lineNumber - 1);
-				return {
-					range: new vscode.Range(line.range.start, line.range.end),
-					hoverMessage: jsonObject[lineNumber].message
-				};
-			});
-			
-			// Apply the decorations to the active text editor
-			editor.setDecorations(successDecorationType, successDecorations);
-			editor.setDecorations(failureDecorationType, failureDecorations);
-			editor.setDecorations(errorDecorationType, errorDecorations);
-			
-		});
+	
 	});
 
 	context.subscriptions.push(runTest);
@@ -115,15 +132,14 @@ export function activate(context: vscode.ExtensionContext) {
 	let clearTest = vscode.commands.registerCommand('school-grader.clearTest', () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
-			vscode.window.showInformationMessage('No active editor found.');
+			vscode.window.showErrorMessage('No active editor found.');
 			return;
 		}
 		editor.setDecorations(successDecorationType, []);
 		editor.setDecorations(failureDecorationType, []);
 		editor.setDecorations(errorDecorationType, []);
+		vscode.window.showInformationMessage('Screen cleared');
 	});
 
 	context.subscriptions.push(clearTest);
 }
-
-export function deactivate() {}
